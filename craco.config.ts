28eb159f44718dd91a1a5edd25c93ef8a394b216
Seok-CH <tjr6677@gmail.com>
2023-02-07 18:06:31 +0900

    fix: 절대경로 import의 버그 수정
    현재 src/index.tsx의 "@pages/index"에서 index를 생략하면 파일을 인식 못하는 현상이 있습니다. 버그인지 잘못된 설정인지 모르겠으나 다음과 같은 이유로 모든 절대경로를 "@/*"로 시작하려고합니다.
    1.외부모듈 중에서 @emotion등 @로 시작하는 것들이 많아 헷갈릴수있다.
    2.위에서 서술한 현상이 있다.
    3.@를 없애고 "pages/*" 같이 절대경로를 쓰는 방법이 있지만 이것 또한 외부모듈과 헷갈릴 수 있다
    그래서 src에서 불러오고 있다는 것을 표시하기 위해 "@/*"로 절대경로 import를 하는 것으로 변경합니다.

--- craco.config.ts
+++ craco.config.ts
@@ -4,10 +4,6 @@ module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@features": path.resolve(__dirname, "src/features"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
-      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
   babel: {
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import TodoApp from "@pages/index";
+import TodoApp from "@/pages";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
